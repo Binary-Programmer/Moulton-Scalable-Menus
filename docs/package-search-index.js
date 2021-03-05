7812abc0365a89de39132bd4038fb89ba93f07fb
Matthew Moulton <30711895+mmoult@users.noreply.github.com>
2021-03-05 14:31:07 -0700

    Remove JavaDocs from Repo
    I have decided to put the include the Javadocs in the release by means of a downloadable zip file. It didn't make sense to me to include the exact docs in with the rest of the repo files since they change often and bloat the source download.

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"moulton.scalable.clickables"},{"l":"moulton.scalable.containers"},{"l":"moulton.scalable.draggables"},{"l":"moulton.scalable.geometrics"},{"l":"moulton.scalable.popups"},{"l":"moulton.scalable.texts"},{"l":"moulton.scalable.utils"},{"l":"moulton.scalable.visuals"}];updateSearchResults();
